ff07100518de1c09e49cb2e4389961e8935c96fd
Krystian Jabłoński <Krystian Jaboski>
2017-11-05 20:39:37 +0100

    fiexs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+let EventEmitter = require('events').EventEmitter;
 let OSInfo = require('./modules/OSInfo');
 OSInfo.print();
 
+let emitter = new EventEmitter();
+emitter.on('beforeCommand', function(instruction){
+    console.log('You wrote: ' + instruction + 'trying to run command.')
+});
+emitter.on('afterCommand', function(){
+    console.log('Finished command');
+});
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     return process.env;
@@ -8,6 +16,7 @@ process.stdin.on('readable', function() {
     let input = process.stdin.read();
     if(input !== null) {
         let instruction = input.toString().trim();
+        emitter.emit('beforeCommand', instruction);
             switch(instruction) {
             case '/exit': {
                 process.stderr.write('Quitting app!\n');
@@ -22,5 +31,6 @@ process.stdin.on('readable', function() {
                 process.stderr.write('Wrong instruction\n');
                 break;
             };
+        emitter.emit('afterCommand');
         };
 });
