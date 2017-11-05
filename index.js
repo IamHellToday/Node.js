89da50212660f408af881898ab0acc0170ea36ff
Krystian Jabłoński <Krystian Jaboski>
2017-11-05 20:50:49 +0100

    formated

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let EventEmitter = require('events').EventEmitter;
-let OSInfo = require('./modules/OSInfo');
+let EventEmitter = require("events").EventEmitter;
+let OSInfo = require("./modules/OSInfo");
 OSInfo.print();
 
 let emitter = new EventEmitter();
-emitter.on('beforeCommand', function(instruction){
-    console.log('You wrote: ' + instruction + 'trying to run command.')
+emitter.on("beforeCommand", function(instruction) {
+  console.log("You wrote: " + instruction + "trying to run command.");
 });
-emitter.on('afterCommand', function(){
-    console.log('Finished command');
+emitter.on("afterCommand", function() {
+  console.log("Finished command");
 });
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    let input = process.stdin.read();
-    if(input !== null) {
-        let instruction = input.toString().trim();
-        emitter.emit('beforeCommand', instruction);
-            switch(instruction) {
-            case '/exit': {
-                process.stderr.write('Quitting app!\n');
-                process.exit();
-                break;
-            }
-            case '/getOSinfo': {
-                getOsInfo();
-                break;
-            }
-            default:
-                process.stderr.write('Wrong instruction\n');
-                break;
-            };
-        emitter.emit('afterCommand');
-        };
-    return process.env;
-    return process.versions;
+process.stdin.setEncoding("utf-8");
+process.stdin.on("readable", function() {
+  let input = process.stdin.read();
+  if (input !== null) {
+    let instruction = input.toString().trim();
+    emitter.emit("beforeCommand", instruction);
+    switch (instruction) {
+      case "/exit": {
+        process.stderr.write("Quitting app!\n");
+        process.exit();
+        break;
+      }
+      case "/getOSinfo": {
+        getOsInfo();
+        break;
+      }
+      default:
+        process.stderr.write("Wrong instruction\n");
+        break;
+    }
+    emitter.emit("afterCommand");
+  }
+  return process.env;
+  return process.versions;
 });
