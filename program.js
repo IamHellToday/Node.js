73e7e771843642e777af76021d7f07f1d99f3a95
Krystian Jabłoński <Krystian Jaboski>
2017-11-05 21:24:50 +0100

    fixes

--- program.js
+++ program.js
@@ -1,22 +1,26 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function(){
+process.stdin.on('readable', function() {
     let input = process.stdin.read();
-    if(input !== null) {
+    if (input !== null) {
         let instruction = input.toString().trim();
-            switch(instruction) {
+
+        switch (instruction) {
             case '/exit': {
                 process.stderr.write('Quitting app!\n');
                 process.exit();
                 break;
             }
-            default:
+            case '/version': {
+                console.log(process.versions);
+                break;
+            }
+            case '/enviroment': {
+                console.log(process.env);
+            }
+            default: {
                 process.stderr.write('Wrong instruction\n');
                 break;
-            };
-        };
-    return process.env;
-    return process.versions;
+            }
+        }
+    }
 });
-
-/*Najpierw działało, przed wrzuceniem switcha. Teraz natomiast, wyrzuca w konsoli [instruction] is not recognized as an internal or external command,
-operable program or batch file.*/
