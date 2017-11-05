5584d7e96947dc3a51ef8d0a754f02f41011d6ec
Krystian Jabłoński <Krystian Jaboski>
2017-11-05 21:59:45 +0100

    fiexs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let EventEmitter = require("events").EventEmitter;
-let OSInfo = require("./modules/OSInfo");
+let OSInfo = require('./modules/OSInfo');
 OSInfo.print();
 
 let emitter = new EventEmitter();
@@ -9,28 +9,42 @@ emitter.on("beforeCommand", function(instruction) {
 emitter.on("afterCommand", function() {
   console.log("Finished command");
 });
-process.stdin.setEncoding("utf-8");
-process.stdin.on("readable", function() {
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+
   let input = process.stdin.read();
   if (input !== null) {
+  
     let instruction = input.toString().trim();
     emitter.emit("beforeCommand", instruction);
+
     switch (instruction) {
-      case "/exit": {
-        process.stderr.write("Quitting app!\n");
+      case '/exit': {
+        process.stderr.write('Quitting app!\n');
         process.exit();
         break;
       }
-      case "/getOSinfo": {
-        getOsInfo();
+      case '/version': {
+        console.log(process.versions);
+        console.log('Choose command between: "/version" "/enviroment" "/exit" "/getOSinfo" ');
         break;
       }
-      default:
-        process.stderr.write("Wrong instruction\n");
+      case '/enviroment': {
+        console.log(process.env);
+        console.log('Choose command between: "/version" "/enviroment" "/exit" "/getOSinfo" ');
         break;
+      }
+      case '/getOSinfo': {
+        getOsInfo();
+        console.log('Choose command between: "/version" "/enviroment" "/exit" "/getOSinfo" ');
+        break;
+      }
+      default: {
+        process.stderr.write('Wrong instruction\n');
+        console.log('Choose command between: "/version" "/enviroment" "/exit" "/getOSinfo" ');
+        break;
+      }
     }
     emitter.emit("afterCommand");
   }
-  return process.env;
-  return process.versions;
 });
--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -2,17 +2,20 @@ let os = require('os');
 let colors = require('colors');
 
 function getOsInfo() {
+
     let type = os.type();
     let release = os.release();
-        if(type === 'Darwin') {
-            type = 'OSX';
-        } else if ( type === 'Windows_NT') {
-            type = 'Windows';
-        } else if (type === 'Linux') {
-            type = 'Linux';
-        };
-        console.log('System:'.grey, type);
-        console.log('Release:'.red, release);
+
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if ( type === 'Windows_NT') {
+        type = 'Windows';
+    } else if (type === 'Linux') {
+        type = 'Linux';
+    };
+
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
 
     let cpu = os.cpus()[0].model;
     console.log('Cpu model:'.cyan, cpu);
@@ -23,5 +26,6 @@ function getOsInfo() {
     let userInfo = os.userInfo();
     console.log('User name:'.magenta, userInfo.username);
     console.log('Home dir'.white, userInfo.homedir);
+    console.log('Choose command between: "/version" "/enviroment" "/exit" "/getOSinfo" ');
 }; 
 exports.print = getOsInfo;
