02ac6b30d4ff046ccaea2154d3c57ed2f8fd6893
Krystian Jabłoński <Krystian Jaboski>
2017-11-05 15:31:51 +0100

    addons

--- program.js
+++ program.js
@@ -1,1 +1,22 @@
-console.log(global);
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function(){
+    return process.env;
+    return process.versions;
+    let input = process.stdin.read();
+    if(input !== null) {
+        let instruction = input.toString().trim();
+            switch(instruction) {
+            case '/exit': {
+                process.stderr.write('Quitting app!\n');
+                process.exit();
+                break;
+            }
+            default:
+                process.stderr.write('Wrong instruction\n');
+                break;
+            };
+        };
+});
+
+/*Najpierw działało, przed wrzuceniem switcha. Teraz natomiast, wyrzuca w konsoli [instruction] is not recognized as an internal or external command,
+operable program or batch file.*/
