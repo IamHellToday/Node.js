a2d1bf08e3e63eb920957db06d478a30474b9932
Krystian Jabłoński <Krystian Jaboski>
2017-11-09 14:25:05 +0100

    fixes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let fs = require('fs');
 
-fs.stat('./cat.jpg', function(err, stats){
-    console.log(stats);
+fs.readdir('./node_modules', 'utf-8', function(err, stats) {
+    console.log(stats.toString());
+    fs.appendFile('components.txt', stats, 'utf-8', function(err) {
+        if (err) {
+            throw err;
+        } else {
+        console.log(`File has been saved including ${stats} components.`);
+        }
+    });
 });
