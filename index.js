af2385ae6a4aa84b4e25f0bd1407979a4f48626d
Krystian Jabłoński <Krystian Jaboski>
2017-11-11 00:27:19 +0100

    fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let http = require('http');
 let fs = require('./filestats');
 
 let server = http.createServer(function(request, response) {
-    response.write(`Module contains ${stats}`);
+    response.write(`Module contains ${fs}`);
     response.end();
 });
 server.listen(9000);
