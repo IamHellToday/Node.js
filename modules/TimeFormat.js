edf6be16794e53102856da37a9b23e7c8d9a2d18
Krystian Jabłoński <Krystian Jaboski>
2017-11-12 20:16:20 +0100

    fixes

--- modules/TimeFormat.js
+++ modules/TimeFormat.js
@@ -1,7 +1,7 @@
 function transformTime (uptime) {
     let hours = Math.floor(uptime / 3600);
     let minutes = Math.floor((uptime - (hours * 3600)) / 60);
-    let seconds = uptime - (hours * 3600) - (minutes * 60);
+    let seconds = Math.floor(uptime - (hours * 3600) - (minutes * 60));
 
     if (hours < 10) {
         hours = "0"+hours;
@@ -10,6 +10,6 @@ function transformTime (uptime) {
     } else if (seconds < 10) {
         seconds = "0"+seconds;
     }
-    return hours+':'+minutes+':'+seconds+'sec';
+    return hours+':'+minutes+':'+seconds+ ' sec';
 };
 module.exports = transformTime;
